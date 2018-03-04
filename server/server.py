850d301d04d0f6447c69d07ec15883c5357589a8
Nathan Goodman <Nathan.something@gmail.com>
2018-03-03 21:46:58 -0500

    updated document functionality, id for document works

--- server/server.py
+++ server/server.py
@@ -1,75 +1,79 @@
 from flask import Flask, jsonify, url_for, redirect, request
 from flask_pymongo import PyMongo
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, abort
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config["MONGO_DBNAME"] = "words_db"
 mongo = PyMongo(app, config_prefix='MONGO')
 APP_URL = "http://127.0.0.1:5000"
 
 class Documents(Resource):
 
-    def get(Resource):
-        data = []
-        cursor = mongo.db.documents.find({}, {"_id": 0, "update_time": 0})
-        for document in cursor:
-            print(document)
-            data.append(document)
-        return jsonify({"response":data})
+    def get(Resource, document_id=None):
+        document_data = []
+        if not document_id:
+            cursor = mongo.db.documents.find({})
+            for document in cursor:
+                document["document_id"] = str(document.pop("_id"))
+                document_data.append(document)
+        else:
+            document_data = mongo.db.documents.find_one({"_id":document_id})
+
+        return jsonify(document_data)
 
     def post(Resource):
         data = request.get_json()
         if not data:
-            return jsonify({"response":"ERROR"})
+            abort(204, message="No Data")
         else:
-            print(data)
+            word_ids = []
             name = data['name']
             language = data['language']
+            created_on = data['createdOn']
             words = data['words']
-            mongo.db.documents.insert({"name":name,"language":language,"words":words})
+            for word in words:
+                word_id = mongo.db.words.find({"text":word['text'],"language":word['language']})
+                if (word_id):
+                    word_ids.append(word_id)
+                else:
+                    word_id = mongo.db.words.insert({"text":word['text'],"language":word['language'],"translation":word['translation'],"isPunctuation":word['isPunctuation']})
+                    word_ids.append(word_id)
+
+            mongo.db.documents.insert({
+                "name":name,
+                "language":language,
+                "createdOn":created_on,
+                "words": []
+            })
+            return jsonify({"response": "SUCCESS"})
+
+    def delete(Resource, document_id=None):
+        if document_id:
+            mongo.db.documents.remove_one({"_id":document_id})
+        else:
+            print('about to remove all documents')
+            mongo.db.documents.remove({},{"_id": 0, "update_time": 0})
+        return jsonify({"response":"SUCCESS"})
 
 class Words(Resource):
     def get(self):
-        data = []
+        word_data = []
         cursor = mongo.db.words.find({}, {"_id": 0, "update_time": 0})
         for word in cursor:
-            print(word)
-            data.append(word)
-        return jsonify({"response": data})
-
-    def post(self):
-        data = request.get_json()
-        if not data:
-            data = {"response": "ERROR"}
-            return jsonify(data)
-        else:
-            text = data['text']
-            language = data['language']
-            familiarity = "UNKNOWN"
-            mongo.db.words.insert({"text":text,"language":language,"familiarity":familiarity})
-            return jsonify({'response': "SUCCESS"})
-
-    # def put(self, registration):
-    #     data = request.get_json()
-    #     mongo.db.student.update({'registration': registration}, {'$set': data})
-    #     return redirect(url_for("students"))
-    #
-    # def delete(self, registration):
-    #     mongo.db.student.remove({'registration': registration})
-    #     return redirect(url_for("students"))
-
+            word_data.append(word)
+        return jsonify(word_data)
 
 class Index(Resource):
     def get(self):
         return redirect(url_for("words"))
 
-
 api = Api(app)
 api.add_resource(Index, "/", endpoint="index")
 api.add_resource(Words, "/words", endpoint="words")
 api.add_resource(Documents, "/documents", endpoint="documents")
-# api.add_resource(Student, "/api/<string:registration>", endpoint="registration")
-# api.add_resource(Student, "/api/department/<string:department>", endpoint="department")
+api.add_resource(Documents, "/documents/<int:document_id>", endpoint="get_document")
 
 if __name__ == "__main__":
     app.run(debug=True)
