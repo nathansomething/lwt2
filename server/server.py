2c2e05777a6d47f8c64e2dc45b620d0dd84b805c
Nathan Goodman <Nathan.something@gmail.com>
2018-03-07 17:05:25 -0500

    added new fields for documents and words

--- server/server.py
+++ server/server.py
@@ -12,7 +12,7 @@ APP_URL = "http://127.0.0.1:5000"
 
 class Documents(Resource):
 
-    def get(Resource, document_id=None):
+    def get(self, document_id=None):
         document_data = []
         if not document_id:
             cursor = mongo.db.documents.find({})
@@ -20,38 +20,27 @@ class Documents(Resource):
                 document["documentId"] = str(document.pop("_id"))
                 document_data.append(document)
         else:
-            document_data = mongo.db.documents.find_one({"name":"Testing"})
+            document_data = mongo.db.documents.find_one({"_id":ObjectId(document_id)})
             document_data["documentId"] = str(document_data.pop("_id"))
 
         return jsonify(document_data)
 
-    def post(Resource):
+    def post(self):
         data = request.get_json()
         if not data:
             abort(204, message="No Data")
         else:
-            word_ids = []
-            name = data['name']
-            language = data['language']
-            created_on = data['createdOn']
-            words_in_document = data['words']
-            for word in words_in_document:
-                lookup_word = mongo.db.words.find_one({"text":word['text'],"language":word['language']})
-                if (lookup_word):
-                    word_ids.append(str(lookup_word["_id"]))
-                else:
-                    word_id = mongo.db.words.insert({"text":word['text'],"language":word['language'],"translation":word['translation'],"isPunctuation":word['isPunctuation']})
-                    word_ids.append(str(word_id))
-
             mongo.db.documents.insert({
-                "name":name,
-                "language":language,
-                "createdOn":created_on,
-                "words": word_ids
+                "name": data['name'],
+                "language": data['language'],
+                "createdOn": data['createdOn'],
+                "orderedWordIds": data['orderedWordIds'],
+                "uniqueWordIds": data['uniqueWordIds'],
+                "numberOfWords": data['numberOfWords']
             })
             return jsonify({"response": "SUCCESS"})
 
-    def delete(Resource, document_id=None):
+    def delete(self, document_id=None):
         if document_id:
             mongo.db.documents.remove_one({"_id":document_id})
         else:
@@ -65,7 +54,8 @@ class Words(Resource):
         word_data = []
         cursor = None
         if word_ids:
-            word_ids = word_ids.split('|')
+            print(word_ids)
+            word_ids = word_ids.split(',')
             word_object_ids = []
             for word_id in word_ids:
                 word_object_ids.append(ObjectId(word_id))
@@ -73,22 +63,30 @@ class Words(Resource):
         else:
             cursor = mongo.db.words.find({})
         for word in cursor:
-            word["wordId"] = str(word.pop("_id"))
+            word["wordId"] = str(word.pop('_id'))
             word_data.append(word)
-        print(word_data)
         return jsonify(word_data)
+
+    def post(self):
+        words = request.get_json()
+        if not words:
+            abort(204, message="No Data")
+        word_ids = []
+        for word in words:
+            check_word = mongo.db.words.find_one({"text":word['text'],"language":word['language']})
+            if not check_word:
+                check_word = mongo.db.words.insert_one({"text":word['text'],"language":word['language'],"translation":word['translation'],"familiarity":1,"isPunctuation":word['isPunctuation']})
+                word_ids.append(str(check_word.inserted_id))
+            else:
+                word_ids.append(str(check_word['_id']))
+        return jsonify(word_ids)
 
     def delete(self):
         mongo.db.words.remove({})
         return jsonify({"response":"SUCCSESS"})
 
-class Index(Resource):
-    def get(self):
-        return redirect(url_for("words"))
-
 api = Api(app)
-api.add_resource(Index, "/", endpoint="index")
-api.add_resource(Words, "/words/", endpoint="all_words")
+api.add_resource(Words, "/words", endpoint="all_words")
 api.add_resource(Words, "/words/<string:word_ids>", endpoint="select_words")
 api.add_resource(Documents, "/documents", endpoint="documents")
 api.add_resource(Documents, "/documents/<string:document_id>", endpoint="get_document")
